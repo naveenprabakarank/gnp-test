cc87540d4769c0421ce68b1e2c5a154e893b942c
naveenprabakarank <naveenp.mkm@gmail.com>
2025-11-16 12:45:09 +0530

    App files

---
+++ src/main/java/com/test/gnp/GnpApplication.java
@@ -0,0 +1,13 @@
+package com.test.gnp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GnpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GnpApplication.class, args);
+	}
+
+}
