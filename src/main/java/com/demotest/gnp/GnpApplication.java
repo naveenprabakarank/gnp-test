1d18ed9f9c629fc49fbe33c5b59af29eeed4bace
naveenprabakarank <naveenp.mkm@gmail.com>
2025-11-16 14:43:07 +0530

    package update

--- src/main/java/com/demotest/gnp/GnpApplication.java
+++ src/main/java/com/demotest/gnp/GnpApplication.java
@@ -1,4 +1,4 @@
-package com.test.gnp;
+package com.demotest.gnp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/demotest/gnp/GnpApplicationTests.java
+++ src/test/java/com/demotest/gnp/GnpApplicationTests.java
@@ -1,4 +1,4 @@
-package com.test.gnp;
+package com.demotest.gnp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
